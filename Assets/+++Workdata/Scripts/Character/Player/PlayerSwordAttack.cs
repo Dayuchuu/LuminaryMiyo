f3011917e4f3be587932a6d886df91742a101fc5
Justin <justinherrmann34@gmail.com>
2025-01-09 13:55:07 +0100

    # Cleaned up the PlayerMovement and PlayerSwordAttack script

--- Assets/+++Workdata/Scripts/Character/Player/PlayerSwordAttack.cs
+++ Assets/+++Workdata/Scripts/Character/Player/PlayerSwordAttack.cs
@@ -6,14 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerSwordAttack : MonoBehaviour
 {
    #region Variables
-
-   [SerializeField] private float attackDistance = 0f;
-
+   
    [SerializeField] private LayerMask attackLayer = 0;
-
-   [SerializeField] private Transform cameraTransform = null;
-
-   [SerializeField] private Vector3 boxCastSize = new Vector3();
 
    private Animator swordAnim = null;
 
@@ -42,25 +36,9 @@ public class PlayerSwordAttack : MonoBehaviour
 
    public void Attack(InputAction.CallbackContext context)
    { 
-      RaycastHit hit = new RaycastHit(); 
-   
       swordAnim.SetTrigger("Attacks");
       
       swordAttackSound.Play();
-
-      // Physics.BoxCast(cameraTransform.position, boxCastSize, cameraTransform.forward * 0.1f, out hit, Quaternion.identity);
-
-      // if (hit.collider != null)
-      // {
-      //    Debug.Log(hit.collider.gameObject.name);
-      // }
-      
-      // if (hit.collider != null && hit.collider.CompareTag("Enemy"))
-      // {
-      //    hit.collider.GetComponent<CharacterBase>().healthPoints--;
-      //    
-      //    hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
-      // }
    }
    #endregion
 } 
